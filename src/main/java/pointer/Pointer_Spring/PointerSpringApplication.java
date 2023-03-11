dd34f3e8b8d1f39c26e976a08b05c0d8e1ded513
12hyeon <hyeonjeong1212@naver.com>
2023-03-11 18:45:40 +0900

    init : create project

---
+++ src/main/java/pointer/Pointer_Spring/PointerSpringApplication.java
@@ -0,0 +1,13 @@
+package pointer.Pointer_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointerSpringApplication.class, args);
+	}
+
+}
