7f2a7d27e6466229e4ef8d8561b66d5ca34b218c
hil <hil679922@gmail.com>
2023-03-16 01:27:54 +0900

    Add: config for swagger

---
+++ src/main/java/pointer/Pointer_Spring/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package pointer.Pointer_Spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(" pointer.Pointer_Spring.swagger"))//pagkage명과  동일
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Backend API")
+                .description("Backend API 문서")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/pointer/Pointer_Spring/config/WebConfig.java
+++ src/main/java/pointer/Pointer_Spring/config/WebConfig.java
@@ -5,18 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /*@Value("${upload.path}")
-    private String uploadPath;
-
-    @Value("${resource.path}")
-    private String resourcePath;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(uploadPath)
-                .addResourceLocations(resourcePath);
-    }*/
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger-ui/")
+                .setViewName("forward:/swagger-ui/index.html");
+    }
+
 }
