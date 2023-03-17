53da9dbba13b5cde988cdcac9b4469f9f51d51b7
hil <hil679922@gmail.com>
2023-03-18 01:50:52 +0900

    Change: annotation according to version

--- src/main/java/pointer/Pointer_Spring/swagger/SwaggerController.java
+++ src/main/java/pointer/Pointer_Spring/swagger/SwaggerController.java
@@ -1,12 +1,14 @@
 package pointer.Pointer_Spring.swagger;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import lombok.Data;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -14,18 +16,15 @@ import java.util.Map;
 
 //Controller
 @RestController
+
 public class SwaggerController {
     @GetMapping(value = "/hello")
-    @ApiOperation(value = "hello, world api", notes = "hello world swagger check")
+    @ApiOperation(value = "hello, world api", notes = "hello world swagger check",  authorizations = {@Authorization(value = "JWT") })
     public String hellowWorld(){
         return "hello, world";
     }
 
     @ApiOperation(value = "test", notes = "테스트입니다")//요청 URL에 매핑된 API 에 대한 설명
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "ok"),
-            @ApiResponse(code = 404, message = "page not found!")
-    })
     @GetMapping(value = "/board")
     public Map<String, String> selectBoard(@ApiParam(value = "샘플번호", required = true, example = "1")
                                            @RequestParam String no) {
@@ -36,12 +35,17 @@ public class SwaggerController {
         return  result;
     }
 
-    @Operation(summary="요약", description="설명")
-    @ApiResponse(code = 200, message="ok")
+    @ApiOperation(value="동작", notes="설명")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = SwaggerTestDto.class))),
+            @ApiResponse(responseCode = "404", description = "500과 동일")
+    })
     @PostMapping("/ex/")
-    public SwaggerTestDto exampleMethod() {
-        return new SwaggerTestDto();
+    public SwaggerTestDto exampleMethod(@RequestBody SwaggerTestDto swaggerTestDto) {
+        return new SwaggerTestDto(swaggerTestDto.getId(), swaggerTestDto.getName());
     }
+
+
 
 }
 
--- src/main/java/pointer/Pointer_Spring/swagger/SwaggerTestDto.java
+++ src/main/java/pointer/Pointer_Spring/swagger/SwaggerTestDto.java
@@ -2,13 +2,17 @@ package pointer.Pointer_Spring.swagger;
 
 
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Schema(name = "SwaggerTestDto", description = "swagger test dto")
 public class SwaggerTestDto {
-    @ApiModelProperty(value="id", name = "id", example="1", dataType = "Long", required=true)
+    @Schema(description = "id", defaultValue = "", allowableValues = {}, example = "1")
     private Long id;
 
-    @ApiModelProperty(value="name", example="예시")
+    @Schema(description = "유저 이름", defaultValue = "", allowableValues = {}, example = "user1")
     private String name;
 }
