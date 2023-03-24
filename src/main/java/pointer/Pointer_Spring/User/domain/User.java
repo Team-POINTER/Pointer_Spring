adb1bd5fc0f8763f4647295af86620a3c445893e
12hyeon <hyeonjeong1212@naver.com>
2023-03-24 18:04:18 +0900

    fix(User): Builder 추가

--- src/main/java/pointer/Pointer_Spring/User/domain/User.java
+++ src/main/java/pointer/Pointer_Spring/User/domain/User.java
@@ -1,49 +1,65 @@
 package pointer.Pointer_Spring.User.domain;
 
-import lombok.*;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import pointer.Pointer_Spring.User.dto.KakaoRequestDto;
-
 import javax.persistence.*;
 
-@Getter
-@NoArgsConstructor
-@Table(name = "user")
-@Entity
-public class User {
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pointer.Pointer_Spring.config.BaseEntity;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "USER")
+public class User extends BaseEntity {
+
+    //    @Getter(AccessLevel.NONE)
     @Id
-    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true)
     private Long userId;
 
-    @Setter
+    @Column(nullable = false)
     private String id;
+    @Column(nullable = false, unique = true)
     private String email;
-    @Setter
-    private String username;
-    @Setter
+    @Column(nullable = false)
     private String nickname;
-    private String password;
 
-    @Enumerated(EnumType.STRING)
+    private String username;
+    private String password;
     private Authority authority;
 
+    public enum Type {
+        APP, KAKAO, APPLE
+    }
+
+    @Column(nullable = false)
+    private Type type;
+
+
     @Builder
+    public User(String id, String email, String nickname, Type type) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.type = type;
+
+    }
+
+    @Builder(builderMethodName = "KakaoBuilder")
+    public User(String email, String id, String nickanme, String username) {
+        this.email = email;
+        this.id = id;
+        this.nickname = nickanme;
+        this.username = username;
+        this.authority = Authority.ROLE_USER;
+    }
+
+    @Builder(builderMethodName = "AuthorityBuilder")
     public User(String email, String password, Authority authority) {
         this.email = email;
         this.password = password;
         this.authority = authority;
     }
-
-    // 카카오
-    @Builder
-    public User(String id, String email, String username, String nickname) {
-        this.id = id;
-        this.email = email;
-        this.username = username;
-        this.nickname = nickname;
-        this.authority = Authority.ROLE_USER;
-    }
-
 }
