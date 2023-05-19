a42bd8fdfc4151459c73e6b3f907f92a055f9bd2
DanielYY95 <asrs02041@naver.com>
2023-05-19 22:19:41 +0900

    Chore openfeign, okkyhttp 라이브러리 추가

--- src/main/java/pointer/Pointer_Spring/PointerSpringApplication.java
+++ src/main/java/pointer/Pointer_Spring/PointerSpringApplication.java
@@ -2,14 +2,17 @@ package pointer.Pointer_Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class PointerSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PointerSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PointerSpringApplication.class, args);
+    }
 
 }
